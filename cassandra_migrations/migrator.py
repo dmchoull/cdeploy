12213fa7627b7eb0bc8b7d4b7bca6ce2bc3060f5
David McHoull <dmchoull@thoughtworks.com>
2014-04-10 15:01:11 -0400

    refactoring

--- cassandra_migrations/migrator.py
+++ cassandra_migrations/migrator.py
@@ -8,6 +8,9 @@ class Migrator:
 
     def run_migrations(self):
         for file_name in os.listdir(self.migrations_path):
-            migration_file = open(os.path.join(self.migrations_path, file_name))
-            migration_content = migration_file.read()
+            migration_content = self._read_migration(file_name)
             self.session.execute(migration_content)
+
+    def _read_migration(self, file_name):
+        migration_file = open(os.path.join(self.migrations_path, file_name))
+        return migration_file.read()
--- cassandra_migrations/test/migrator_test.py
+++ cassandra_migrations/test/migrator_test.py
@@ -2,17 +2,21 @@ import unittest
 from mock import *
 from cassandra_migrations import Migrator
 
+migration_1_content = open('./migrations/001_create_users.cql').read()
+migration_2_content = open('./migrations/002_add_firstname.cql').read()
 
 class MigratorTests(unittest.TestCase):
+    def setUp(self):
+        self.session = MagicMock()
+        self.session.execute = MagicMock()
+
+        self.migrator = Migrator('./migrations', self.session)
+
     def test_it_should_initially_apply_all_the_migrations(self):
-        session = MagicMock()
-        session.execute = MagicMock()
+        self.migrator.run_migrations()
 
-        migrator = Migrator('./migrations', session)
-        migrator.run_migrations()
-
-        expected = [call('CREATE TABLE users'), call('ALTER TABLE users ADD firstname text')]
-        self.assertEqual(session.execute.call_args_list, expected)
+        expected = [call(migration_1_content), call(migration_2_content)]
+        self.assertEqual(self.session.execute.call_args_list, expected)
 
 
 def main():
