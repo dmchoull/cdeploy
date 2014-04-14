b2d5cc7d78b83a3dc3b24ee380e4be387602065b
David McHoull <dmchoull@thoughtworks.com>
2014-04-14 15:48:04 -0400

    Added more info to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name='cdeploy',
-      version='0.8',
-      packages=find_packages(),
-      test_suite = "cdeploy.test",
-      entry_points={
-          'console_scripts': [
-              'cdeploy = cdeploy.migrator:main'
-          ]
-      }
+setup(
+    name='cdeploy',
+    version='0.8',
+    description='A tool for managing Cassandra schema migrations',
+    author='David McHoull',
+    author_email='dmchoull@gmail.com',
+    url='https://github.com/dmchoull/cdeploy',
+    keywords=['cassandra', 'migrations'],
+    packages=find_packages(),
+    test_suite="cdeploy.test",
+    entry_points={
+        'console_scripts': [
+            'cdeploy = cdeploy.migrator:main'
+        ]
+    }
 )
