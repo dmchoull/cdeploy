73f53498f8678997870fa4dc0726f774dacd0b67
David McHoull <dmchoull@thoughtworks.com>
2014-04-15 16:21:49 -0400

    Updated version for 1.0 release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='cdeploy',
-    version='0.8',
+    version='1.0',
     description='A tool for managing Cassandra schema migrations',
     author='David McHoull',
     author_email='dmchoull@gmail.com',
     license='http://www.apache.org/licenses/LICENSE-2.0',
     url='https://github.com/dmchoull/cdeploy',
-    download_url='https://github.com/dmchoull/cdeploy/archive/v0.8.tar.gz',
+    download_url='https://github.com/dmchoull/cdeploy/archive/v1.0.tar.gz',
     keywords=['cassandra', 'migrations'],
     packages=find_packages(),
     install_requires=['PyYAML', 'cassandra-driver'],
